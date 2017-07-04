8877f2b43fdfd9b43d90c2ec9784c30055c2b82d
Christopher Choi <cdddchris@gmail.com>
2017-07-03 22:39:36 -0700

    Initial commit

---
+++ udemy.rb
@@ -0,0 +1,24 @@
+
+# Local Variable
+# 10.times { |x| x = 10; p x}
+
+# Global Variable
+# 10.times do
+#   $x = 10
+# end
+#
+# p $x
+
+# Instance Variable
+# @batting_average  = 300
+
+# Constant
+# TEAM = "Angels"
+# TEAM = "Athletics"
+#
+# p TEAM
+
+#Class Variable
+# class MyClass
+#   @@teams = ["A's", "Tigers"]
+# end

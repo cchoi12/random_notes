c38b20e098ffd1bc118be7dd37ace315014d4e61
Christopher Choi <cdddchris@gmail.com>
2017-07-05 00:42:17 -0700

    add in some notes

---
+++ string_notes.rb
@@ -0,0 +1,9 @@
+str = " The dog was brown and wasn't really all that friendly "
+
+# .strip
+# strip removes all the white space in order to clean up the string.
+
+# .split
+# Can split all the words in a string into an array.
+
+# str.split(//).size
